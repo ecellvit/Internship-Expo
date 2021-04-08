81343f3f07325bc1cfbf5aea5ccf93b18ca335a9
divit-agrawal <73773075+divit-agrawal@users.noreply.github.com>
2021-04-08 15:54:06 +0530

    feat: login-signup page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginSignup from "./Pages/login-singup/login-signup";
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
